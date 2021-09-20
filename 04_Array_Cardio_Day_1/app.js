10c78937370c4c7d607a9ac60abf7ad78d1b9594
Onur Simsek <onur.simsekk3@gmail.com>
2021-09-20 16:41:37 +0300

    Add solution of Reduce exercise

--- 04_Array_Cardio_Day_1/app.js
+++ 04_Array_Cardio_Day_1/app.js
@@ -104,5 +104,14 @@ console.log(sortedByLastName);
 
 // 8. Reduce Exercise
 // Sum up the instances of each of these
-const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
+const data = ['bus', 'bus', 'car', 'car', 'truck', 'truck', 'bus', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
 
+const vehicles = data.reduce((obj, item) => {
+  if (!obj[item]) {
+    obj[item] = 0;
+  }
+  obj[item]++;
+  return obj;
+}, {});
+
+console.log(vehicles);
