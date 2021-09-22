0e1e564210dc8f1f94c2a6e182d344549aea4a50
Onur Simsek <onur.simsekk3@gmail.com>
2021-09-22 23:21:23 +0300

    Add fetch

--- 06_Ajax_Type_Ahead/app.js
+++ 06_Ajax_Type_Ahead/app.js
@@ -1,1 +1,6 @@
-console.log("test");
+const cities = [];
+
+fetch('https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json')
+  .then(response => response.json())
+  .then(data => cities.push(...data));
+  console.log(cities);
