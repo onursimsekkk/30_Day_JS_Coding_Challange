128fcb4db3230bfa93adca0fb02b6f060ad6d6c9
Onur Simsek <onur.simsekk3@gmail.com>
2021-09-28 12:16:05 +0300

    Add findMatches function

--- 06_Ajax_Type_Ahead/app.js
+++ 06_Ajax_Type_Ahead/app.js
@@ -4,3 +4,11 @@ fetch('https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf2
   .then(response => response.json())
   .then(data => cities.push(...data));
   console.log(cities);
+
+function findMatches(wordToMatch, cities) {
+  return cities.filter(place => {
+    // filter with regex
+    const regex = new RegExp(wordToMatch, 'gi');
+    return place.city.match(regex) || place.state.match(regex)
+  });
+}
